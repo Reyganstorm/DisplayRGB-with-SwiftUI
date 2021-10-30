15ba774026cec04590a11ef92773f4b1c2359a6d
Ruslan <ruslan.shtybaev@gmail.com>
2021-10-30 20:19:02 +0300

    Initial Commit

---
+++ DisplayRGB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DisplayRGB
+//
+//  Created by Руслан Штыбаев on 30.10.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DisplayRGB/DisplayRGBApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisplayRGBApp.swift
+//  DisplayRGB
+//
+//  Created by Руслан Штыбаев on 30.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct DisplayRGBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
